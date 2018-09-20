0f0755a31527445b76da35333c1f0110f7ece9f0
Павел Тамилин <you@example.com>
2018-09-21 02:20:35 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var res = 0, mnoz = 5;
+  while(number >= mnoz)
+  {
+    res+=Math.floor(number/mnoz);
+    mnoz*=5;
+  }
+  return res;
 }
